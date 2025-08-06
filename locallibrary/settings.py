cff02e9e0d98372c847887e025a0bbcf5c703ce6
Yu-Te Kuo <yutekuo817@gmail.com>
2025-08-06 12:06:50 +0800

    update allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -36,7 +36,8 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yutekuo.pythonanywhere.com", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://yutekuo.pythonanywhere.com"]
 
 
 # Application definition
